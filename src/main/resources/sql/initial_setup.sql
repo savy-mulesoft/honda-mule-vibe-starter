8391dd877630b4b8130730e1d7cd521760fced23
savy-mulesoft <sarvarth.bhatnagar@salesforce.com>
2026-01-27 14:09:46 -0500

    DBA friendly version

--- src/main/resources/sql/initial_setup.sql
+++ src/main/resources/sql/initial_setup.sql
@@ -1,7 +1,6 @@
 CREATE SCHEMA mule_hkthn_0226;
 
-
-CREATE TABLE IF NOT EXISTS mule_hkthn_0226.prdt_ctgry_12345 (
+CREATE TABLE IF NOT EXISTS mule_hkthn_0226.PRDT_CTGRY (
     product_line                  VARCHAR(10)   NOT NULL,
     product_category_code         VARCHAR(50)   NOT NULL,
     product_category_group_code   VARCHAR(50)   NOT NULL,
@@ -15,12 +14,15 @@ CREATE TABLE IF NOT EXISTS mule_hkthn_0226.prdt_ctgry_12345 (
     business_key_delete_indicator CHAR(1),
     last_action_code              CHAR(1),
 
+    created_by                    VARCHAR(50)   NOT NULL,
+
     created_utc TIMESTAMP NOT NULL DEFAULT CURRENT TIMESTAMP,
     updated_utc TIMESTAMP NOT NULL DEFAULT CURRENT TIMESTAMP,
 
-    CONSTRAINT PK_prdt_ctgry_12345 PRIMARY KEY (
+    CONSTRAINT PK_PRDT_CTGRY PRIMARY KEY (
         product_line,
         product_category_code,
-        product_category_group_code
+        product_category_group_code,
+        created_by
     )
 );
